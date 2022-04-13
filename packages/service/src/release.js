f59ae31a4496b6b9869fadcc630ba5a13d6f5410
zhangyt <326115956@qq.com>
2022-04-13 10:57:53 +0800

    [v10002] feat(1000214):release代码调整

--- packages/service/src/release.js
+++ packages/service/src/release.js
@@ -45,7 +45,7 @@ module.exports = class NebulaCliRelease {
     const rawBranchs = execa.commandSync("git branch -l").stdout.split("\n");
     const normalizeBranchs = rawBranchs.map((branch) => branch.replace("* ", "").trim());
     const releaseBranchs = normalizeBranchs.filter((branch) => {
-      return /^.+v[0-9]+.[0-9]+.[0-9]+/.test(branch);
+      return /^feature/.test(branch);
     });
 
     const { source } = await inquirer.prompt([
@@ -191,12 +191,12 @@ module.exports = class NebulaCliRelease {
     execa.commandSync(`git pull origin ${source}`).stdout;
 
     const separator = `|-${version}-|`;
-    const command = `git log --grep=\\[${version}\\] --pretty=format:%s${separator}%cd`;
+    const command = `git log --grep=\\[${version}\\] --pretty=format:%s${separator}%cd${separator}%an`;
     const commits = execa
       .commandSync(command)
       .stdout.split("\n")
       .map((commit) => {
-        const [subject, date] = commit.split(separator);
+        const [subject, date, author] = commit.split(separator);
         const [type, id, ...contents] = subject.replace(/\[v.+\] ([a-z]+)(\([0-9,]+\))?:(.+)/, "$1|$2|$3").split("|");
 
         const tid = id ? id.replace(/\(|\)/g, "") : undefined;
@@ -204,7 +204,7 @@ module.exports = class NebulaCliRelease {
         let lastContent = commitContent.split(" ").pop() || "";
         const link = /[a-zA-z]+:\/\/[^\s]*/.test(lastContent) ? lastContent : "";
         commit = link ? commitContent.replace(link, "") : commitContent;
-        return { type, tid, version, date, commit, link, subject };
+        return { type, tid, version, date, commit, link, subject, author };
       })
       .filter((commit) => {
         return commit.commit;
@@ -230,10 +230,16 @@ module.exports = class NebulaCliRelease {
     const commits = this.getReleaseCommits();
 
     // 提交记录表格数据展示
-    const preset = [chalk.bold("Type"), chalk.bold("Date"), chalk.bold("TapdId"), chalk.bold("Commit")];
+    const preset = [
+      chalk.bold("Type"),
+      chalk.bold("Date"),
+      chalk.bold("TapdId"),
+      chalk.bold("Commit"),
+      chalk.bold("Author"),
+    ];
 
     const dataList = Array.from(commits, (commitContent) => {
-      let { type, tid, date, commit } = commitContent;
+      let { type, tid, date, commit, author } = commitContent;
       const typeBgColors = {
         feat: chalk.bgGreen,
         fix: chalk.bgRed,
@@ -246,7 +252,7 @@ module.exports = class NebulaCliRelease {
       date = format(new Date(date), "yyyy-MM-dd");
       tid = tid ? chalk.magenta(` ${tid} `) : "";
 
-      return [type, date, tid, commit];
+      return [type, date, tid, commit, author];
     });
 
     if (dataList.length > 0) {
@@ -271,19 +277,6 @@ module.exports = class NebulaCliRelease {
 
   async createReleaseTag() {
     const { target, version } = this.state;
-    // 测试环境和灰度环境需要简短的发布概要
-    let description = "";
-    if (target === ReleaseEnv.Sit) {
-      const { desc } = await inquirer.prompt([
-        {
-          type: "input",
-          name: "desc",
-          message: "版本发布概要描述(数字字母下划线):",
-          default: "feat",
-        },
-      ]);
-      description = desc;
-    }
 
     const prefixs = {
       [ReleaseEnv.Dev]: "dev",
@@ -307,7 +300,7 @@ module.exports = class NebulaCliRelease {
       case ReleaseEnv.Prod:
         return `${prefix}-${version}-${index}`;
       case ReleaseEnv.Sit:
-        return `${prefix}-${version}-${index}_${description}`;
+        return `${prefix}-${version}-${index}`;
       case ReleaseEnv.Beta:
         return `${prefix}-${version}-${index}_beta`;
       case ReleaseEnv.Dev:
@@ -319,11 +312,15 @@ module.exports = class NebulaCliRelease {
   async createTapdId() {
     const { target } = this.state;
     const commits = this.getReleaseCommits();
-    let tapdIdList = commits
-      .filter((commit) => commit.tid)
+    let featIdList = commits
+      .filter((commit) => commit.tid && commit.type === "feat")
       .map((commit) => commit.tid)
       .join(",");
-    if (tapdIdList) return tapdIdList;
+    let fixIdList = commits
+      .filter((commit) => commit.tid && commit.type === "fix")
+      .map((commit) => commit.tid)
+      .join(",");
+    if (featIdList || fixIdList) return { featIdList, fixIdList };
 
     if (target === ReleaseEnv.Dev) return;
 
@@ -335,7 +332,7 @@ module.exports = class NebulaCliRelease {
         default: "0000000",
       },
     ]);
-    return desc;
+    return { featIdList: desc };
   }
 
   async createTapdContents() {
@@ -358,15 +355,14 @@ module.exports = class NebulaCliRelease {
   async megreReleaseCode() {
     const { source, target, version } = this.state;
     const releaseTag = await this.createReleaseTag();
-    let description = releaseTag;
-    if (target === ReleaseEnv.Sit) {
-      description = description.replace(/.*_/, "");
-    }
 
-    const tapdIdList = await this.createTapdId();
+    const { featIdList, fixIdList } = await this.createTapdId();
     const tapdContents = await this.createTapdContents();
+    const tapdIdArr = [];
+    if (featIdList) tapdIdArr.push(`需求ID:${featIdList}`);
+    if (fixIdList) tapdIdArr.push(`缺陷ID:${fixIdList}`);
 
-    const releaseMsgs = [`版本:${version}`, `描述:${description}`, `需求ID:${tapdIdList}`, `简易描述:${tapdContents}`];
+    const releaseMsgs = [`版本:${version}`, ...tapdIdArr, `简易描述:${tapdContents}`];
 
     if (target !== ReleaseEnv.Dev) {
       console.log(chalk.bgGreen.white(`\n ${releaseTag} \n`));
@@ -399,49 +395,58 @@ module.exports = class NebulaCliRelease {
     console.log("\n");
     console.log(`\n标签:${releaseTag}`);
 
-    if (target === ReleaseEnv.Prod || target === ReleaseEnv.Beta) {
-      commandSync(`git push`);
-      commandSync(`git push origin ${releaseTag}`);
-      commandSync(`git checkout ${this.state.source}`);
-      const str = target === ReleaseEnv.Prod ? "线上" : "灰度";
-      console.log(`${chalk.green(`\n${str}环境发布标签:${releaseTag}，请到对应平台发布(https://optimus.737.com/)`)}\n`);
-      if (target === ReleaseEnv.Prod) {
-        const list = ["product", "sit", "develop"];
-        list.forEach((branch) => {
-          commandSync(`git checkout ${branch}`);
-          commandSync(`git pull`);
-          commandSync(`git merge ${this.state.source}`);
-          commandSync(`git push`);
-        });
-        commandSync(`git checkout ${this.state.source}`);
-        commandSync(`git push`);
-      }
-      return;
-    }
-
-    console.log(`\n${chalk.bgGreen.white("代码发布提示")}\n`);
-    const sitTips = [
-      "1.合并代码和新增标签完成，代码暂未推送到服务器",
-      "2.如果需要请与相关人员确认后在推送代码到服务器",
-    ];
-    console.log(`${chalk.yellow(sitTips.join("\n"))}`);
-
-    const { confirm } = await inquirer.prompt([
-      {
-        type: "list",
-        name: "confirm",
-        message: `确认没有问题直接推送到发布到服务器：`,
-        choices: [
-          { name: "确认无误直接发布", value: true },
-          { name: "暂未确认停止发布", value: false },
-        ],
-      },
-    ]);
-    if (!confirm) return;
+    // if (target === ReleaseEnv.Prod || target === ReleaseEnv.Beta) {
+    const branchName = {
+      [ReleaseEnv.Dev]: "开发",
+      [ReleaseEnv.Sit]: "测试",
+      [ReleaseEnv.Beta]: "灰度",
+      [ReleaseEnv.Prod]: "线上",
+    };
     commandSync(`git push`);
     commandSync(`git push origin ${releaseTag}`);
     commandSync(`git checkout ${this.state.source}`);
-    console.log(`${chalk.green("\n代码发布完成")}\n`);
+    console.log(
+      `${chalk.green(
+        `\n${branchName[ReleaseEnv]}环境发布标签:${releaseTag}，请到对应平台发布(https://optimus.737.com/)`,
+      )}\n`,
+    );
+    if (target === ReleaseEnv.Prod) {
+      const list = ["product", "sit", "develop"];
+      list.forEach((branch) => {
+        commandSync(`git checkout ${branch}`);
+        commandSync(`git pull`);
+        commandSync(`git merge ${this.state.source}`);
+        commandSync(`git push`);
+      });
+      commandSync(`git checkout ${this.state.source}`);
+      commandSync(`git push`);
+    }
+    // return;
+    // }
+
+    // console.log(`\n${chalk.bgGreen.white("代码发布提示")}\n`);
+    // const sitTips = [
+    //   "1.合并代码和新增标签完成，代码暂未推送到服务器",
+    //   "2.如果需要请与相关人员确认后在推送代码到服务器",
+    // ];
+    // console.log(`${chalk.yellow(sitTips.join("\n"))}`);
+
+    // const { confirm } = await inquirer.prompt([
+    //   {
+    //     type: "list",
+    //     name: "confirm",
+    //     message: `确认没有问题直接推送到发布到服务器：`,
+    //     choices: [
+    //       { name: "确认无误直接发布", value: true },
+    //       { name: "暂未确认停止发布", value: false },
+    //     ],
+    //   },
+    // ]);
+    // if (!confirm) return;
+    // commandSync(`git push`);
+    // commandSync(`git push origin ${releaseTag}`);
+    // commandSync(`git checkout ${this.state.source}`);
+    // console.log(`${chalk.green("\n代码发布完成")}\n`);
   }
 
   async run() {
